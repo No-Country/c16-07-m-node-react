7b67da8c0af3bd042a1ee48623351f75531da8e2
Jesus Enrique Perez <28enriqueperez@gmail.com>
2024-02-15 19:41:22 -0400

    Delete old version of users controller and router

--- backend/src/router.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from "express";
-import usersController from "./controllers/users-controller";
-
-export default (app: Application) => {
-    app.use("/api/users", usersController);
-}
