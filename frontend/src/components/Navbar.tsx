16e7e571d8db5b8a9e5039d9de979b092333ef00
Diego Leonardo Gloria Salas <dlgloriasalas@gmail.com>
2024-02-20 11:03:17 -0300

    primer cambio navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,29 +1,41 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png"
 
 export default function Navbar() {
-  const navbarStyle = {
-    backgroundColor: "whitesmoke",
-    color: "black",
-    // padding: "10px",
-    display: "block",
-
-    // Agrega más propiedades de estilo según lo necesites
-  };
-  return (
-    <div className="navbar" style={navbarStyle}>
-      <nav>
-        <Link to="/">
-          <img src="/logo.png" alt="logo" />
-        </Link>
-        <ul>
-          <li style={{ display: "inline" }}>
-            <Link to="/Login">Login</Link>
-          </li>
-          <li>
-            <Link to="/Home">Home</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+  
+    return (
+      <div className="bg-green-100 py-4 px-4 w-full">
+        <nav className="container mx-auto flex items-center justify-between text-xl">
+          <ul className="flex space-x-4">  
+            <li>
+              <Link to="/" className="text-green-700 hover:text-green-950 text-2xl">
+                <img src={logo} alt="logo" className="w-9 inline p-auto" />            
+              </Link>         
+            </li>
+            <li>
+              <Link to="/" className="text-green-700 hover:text-green-950 text-2xl font-bold">Acompañar + </Link>                
+            </li>            
+          </ul>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/Home" className="text-green-700 hover:text-green-950">Home</Link>
+            </li>
+            <li>
+              <Link to="/" className="text-green-700 hover:text-green-950">Eventos</Link>
+            </li>
+            <li>
+              <Link to="/" className="text-green-700 hover:text-green-950">Acompañar</Link>
+            </li>
+          </ul>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/Login" className="text-green-700 hover:text-green-950">Login</Link>
+            </li>
+            <li>
+              <Link to="/" className="text-green-700 hover:text-green-950">Ayuda</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
 }
