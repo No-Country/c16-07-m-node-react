edd1aef2c075341dabf39e5de7e0a6bb2e6155a5
Jesus Enrique Perez <28enriqueperez@gmail.com>
2024-02-26 11:57:10 -0400

    Prueba para despliegue en Render. Agrega de nuevo archivos de get all users

--- backend/src/routes/Users.ts
+++ backend/src/routes/Users.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
 import  deleteUserByIdHandler from "../handlers/users/deleteUsersByIdHandler";
+import getAllUsersHandler from "../handlers/users/getAllUsersHandler";
 import  getUserByIdHandler from "../handlers/users/getUserByIdHandler";
 import  getUserByEmailHandler from "../handlers/users/getUserByEmailHandler";
 import  createUserHandler from "../handlers/users/createUserHandler";
@@ -12,7 +13,7 @@ const userRouter = Router();
 userRouter.post("/login", passport.authenticate('local', { session: false }), logUserInHandler);
 userRouter.get("/mail", getUserByEmailHandler);
 userRouter.get("/:id",getUserByIdHandler);
-// userRouter.get("/",getAllUsersHandler);
+userRouter.get("/",getAllUsersHandler);
 userRouter.post("/", createUserHandler);
 userRouter.put("/:id",updateUserHandler);
 userRouter.delete("/:id",deleteUserByIdHandler);
