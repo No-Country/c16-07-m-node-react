d97a73d5764d3dfb9ad145a7a7c2cb778d30497d
Aracely Cruz <yuki_oh91@hotmail.com>
2024-02-27 11:13:15 -0600

    Actualizar email,password,interests, purposes  controlador para updateUser

--- backend/src/controllers/users/updateUser.ts
+++ backend/src/controllers/users/updateUser.ts
@@ -1,6 +1,8 @@
-import User  from  '../../models/User';
+import boom from "@hapi/boom";
+import bcrypt from "bcrypt"
+import {User,Interest, Purpose} from "../../db"
 import { TUser } from "../../types/TUser";
- export default async function updateUser(
+export default async function updateUser(
     userId: number,
     userData: Partial<TUser>, // Datos del usuario
     purposeIds: number[], // IDs de propósitos
@@ -8,32 +10,59 @@ import { TUser } from "../../types/TUser";
   ) {
     try {
         // Actualiza el usuario con los datos proporcionados
-        const user = await User.findByPK(userId);
+        const user = await User.findByPk(userId) as any;
         if (!user) {
-          throw new Error("Usuario no encontrado");
+          throw boom.notFound("No se encontró el usuario con el id provisto");
+        }
+        
+        if(userData.password){
+            userData.password = await bcrypt.hash(userData.password, 10);
+        }
+      
+        if (userData.email && userData.email !== user.email) {
+        
+            const emailAlreadyExist = await User.findOne({
+                where: {
+                    email: userData.email,
+                }
+            });
+
+            if (emailAlreadyExist) {
+                throw boom.badRequest("Ya existe un usuario con ese email");
+            }
+            user.email = userData.email;
+          
+        }
+     
+        
+        if (userData.interestIds && userData.interestIds.length > 0) {
+            const interestsToSet = [];
+        
+            for (let interestId of userData.interestIds) {
+                const interest = await Interest.findByPk(interestId);
+        
+                if (interest) {
+                    interestsToSet.push(interest);
+                } else {
+                    throw boom.notFound(`No existe el interés con ese ID`);
+                }
+            }
+        
+            await user.setInterests(interestsToSet);
+        }
+        
+        if (userData.purposeId) {
+            const purpose = await Purpose.findByPk(userData.purposeId);
+            user.setPurpose(purpose);
         }
     
-        await user.update(userData);
+       
+        
+     return await user.save(userData);
     
-        // Asocia los propósitos e intereses con el usuario
-        await user.setPurposes(purposeIds);
-        await user.setInterests(interestIds);
-    
-        return user;
-      } catch (error) {
+     
+    } catch (error) {
         throw error;
       }
     
- }
-/*import { User } from "../../db";
-
-
-export default async function updateUser(userId: number, newData: TUser) {
-    //Todo: Chequear si se está cambiando el password para hacer nuevo hash
-    
-    const user = await User.update({
-        ...newData
-    }, { where: { id: userId } });
-    
-    return user
-}*/
+    }
