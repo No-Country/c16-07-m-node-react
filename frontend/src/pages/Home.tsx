a1e91d8c6f143cf3c2a2ff9d234f7092cd46bc00
Diego Leonardo Gloria Salas <dlgloriasalas@gmail.com>
2024-02-28 14:29:33 -0300

    home temporal

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,27 +2,24 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="bg-secondary-content bottom-20 min-h-screen w-full text-primary-content" >
+    <main className="bg-secondary-content bottom-20 min-h-screen w-full text-sky-900" >
 
-      <section className="flex justify-center flex-col w-full h-fit" style={{ backgroundImage: 'url("../public/assets/image/banner.png")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
-        <div className="bg-neutral-content">
-          <img src="../public/assets/image/logo_app_acompanar.png" className="m-auto" />
-        </div>
-        <div className="card bg-accent shadow-xl max-h-64 mx-auto my-40">        
-          <div className="card-body items-center text-center">
-            <h2 className="card-title text-2xl">Conectando Generaciones</h2>
-            <p>Un puente social para adultos mayores</p>
-            <div className="card-actions">
-              <button className="btn btn-info">Únete ahora</button>
+      <section className="flex items-center flex-col w-full h-fit" style={{ backgroundImage: 'url("../public/assets/image/banner.png")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>        
+        <div className="card bg-white shadow-xl my-40 min-w-72 mx-10 ">        
+          <div className="card-body items-center text-center text-sky-900">
+            <h2 className="card-title text-4xl font-extrabold max-w-96">Conectando Generaciones</h2>
+            <p className="my-4">Un puente social para adultos mayores</p>
+            <div className="card-actions w-5/6">
+              <button className="btn bg-amber-500 hover:bg-amber-300 active:bg-amber-400 w-full font-bold">Únete Ahora</button>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="flex flex-wrap justify-evenly text-primary my-10">
+      <section className="flex flex-wrap justify-evenly my-10">
         <div className="my-auto text-center">
           <h2 className="text-2xl font-semibold m-4">Eventos</h2>          
-          <p className="text-xl p2 mb-4">Aqui encontrarás todos los eventos de la comunidad</p>
+          <p className="text-xl p-2 mb-4">Aqui encontrarás todos los eventos de la comunidad</p>
         </div>
         <div className="carousel carousel-center max-w-md p-1 space-x-4 bg-neutral rounded-box">
           <div className="carousel-item h-96 w-96">
@@ -40,7 +37,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="w-full h-fit text-primary my-10">
+      <section className="w-full h-fit my-10">
         <div className="m-auto text-center">
           <h2 className="text-2xl font-semibold m-4">Acompañar</h2>
           <p className="text-xl p-2">Programa encuentros, salidas o simplemente charlas para brindar o recibir compañía.</p>
@@ -70,15 +67,15 @@ export default function Home() {
         </div>
       </section>              
 
-      <section className="bg-accent w-full h-fit py-10">
-        <div className="m-auto text-center">
+      <section className="bg-sky-700 text-white w-full h-fit py-10 pb-40">
+        <div className="text-center my-6">
           <h2 className="text-2xl font-semibold m-4">Testimonios</h2>
           <p className="text-xl p-2">Conoce la opinion de nuestra comunidad.</p>
         </div>
         <div className="flex flex-wrap justify-center ">
-          <article className="w-72 bg-neutral rounded-xl shadow-xl m-2">
+          <article className="w-72 bg-sky-500 rounded-xl shadow-2xl m-2">
             <div className="h-32 grid place-items-center">
-              <div className="border-2 border-stone-800 p-1.5 rounded-full">
+              <div className="border-2 border-black p-1.5 rounded-full">
                 <div className="relative h-20 w-20 rounded-full overflow-hidden">
                   <img
                     className="absolute w-full h-full top-0 left-0 object-cover"
@@ -87,17 +84,17 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="text-white text-center bg-neutral p-5 rounded-xl">
+            <div className="text-white text-center bg-sky-700 p-5 rounded-xl">
               <p className="text-2xl font-semibold">Juan Pérez</p>
-              <label className="text-xs text-stone-400 mb-2">Jubilado</label>
-              <p className="text-stone-300 mb-2">
+              <label className="text-xs mb-2">Jubilado</label>
+              <p className="mb-2">
                 “Gracias a Acompañar+, siento que pertenezco”
               </p>            
             </div>
           </article>
-          <article className="w-72 bg-neutral rounded-xl shadow-xl m-2">
+          <article className="w-72 bg-sky-500 rounded-xl shadow-2xl m-2">
             <div className="h-32 grid place-items-center">
-              <div className="border-2 border-stone-800 p-1.5 rounded-full">
+              <div className="border-2 border-black p-1.5 rounded-full">
                 <div className="relative h-20 w-20 rounded-full overflow-hidden">
                   <img
                     className="absolute w-full h-full top-0 left-0 object-cover"
@@ -106,17 +103,17 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="text-white text-center bg-neutral p-5 rounded-xl">
+            <div className="text-white text-center bg-sky-700 p-5 rounded-xl">
               <p className="text-2xl font-semibold">Ana Gómez</p>
-              <label className="text-xs text-stone-400 mb-2">Profesora</label>
-              <p className="text-stone-300 mb-2">
+              <label className="text-xs mb-2">Profesora</label>
+              <p className="mb-2">
                 “Esta plataforma ha cambiado mi vida social”
               </p>            
             </div>
           </article>
-          <article className="w-72 bg-neutral rounded-xl shadow-xl m-2">
+          <article className="w-72 bg-sky-500 rounded-xl shadow-2xl m-2">
             <div className="h-32 grid place-items-center">
-              <div className="border-2 border-stone-800 p-1.5 rounded-full">
+              <div className="border-2 border-black p-1.5 rounded-full">
                 <div className="relative h-20 w-20 rounded-full overflow-hidden">
                   <img
                     className="absolute w-full h-full top-0 left-0 object-cover"
@@ -125,10 +122,10 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="text-white text-center bg-neutral p-5 rounded-xl">
+            <div className="text-white text-center bg-sky-700 p-5 rounded-xl">
               <p className="text-2xl font-semibold">Luis Ramírez</p>
-              <label className="text-xs text-stone-400 mb-2">Comerciante</label>
-              <p className="text-stone-300 mb-2">
+              <label className="text-xs mb-2">Comerciante</label>
+              <p className="mb-2">
                 “Siempre hay algo nuevo que descubrir aquí”
               </p>            
             </div>
