27499cdd10a99a54376a93b33544ff31afe86760
Diego Leonardo Gloria Salas <dlgloriasalas@gmail.com>
2024-03-04 11:06:47 -0300

    creado archivo paralelo al home, landing

--- frontend/src/route.config.ts
+++ frontend/src/route.config.ts
@@ -3,6 +3,7 @@ import Login from "./pages/login";
 import MostrarOldman from "./oldMen/MostrarOldman";
 import Eventos from "./pages/Eventos";
 import Ayuda from "./pages/Ayuda";
+import Landing from "./pages/Landing";
 
 const rutas = [
    { path: "/oldMen/mostrar", element: MostrarOldman },
@@ -10,5 +11,6 @@ const rutas = [
    { path: "/Login", element: Login },
    { path: "/Eventos", element: Eventos },
    { path: "/Ayuda", element: Ayuda },
+   { path: "/Landing", element: Landing},
 ];
 export default rutas;
