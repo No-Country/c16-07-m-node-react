41e0e8177aa11c1a7455bb75b674a00fec4c7395
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-03-04 22:07:37 -0300

    Handler y controllador de relacionar evento con usuario

---
+++ backend/src/controllers/events/associateEvent.ts
@@ -0,0 +1,26 @@
+import { User, Event } from "../../db";
+
+
+
+const associateEvent = async (userId: number, eventId: number) => {
+  try {
+
+    const user = await User.findByPk(userId);
+    const event = await Event.findByPk(eventId);
+
+    if (!user || !event) {
+      throw new Error("Usuario o evento no encontrado");
+    }
+
+
+    await user.addEvent(event);
+
+    return { success: true, message: "Evento asociado al usuario correctamente" };
+  } catch (error) {
+    console.error("Error al asociar evento al usuario:", error);
+    return { success: false, message: "Error al asociar evento al usuario" };
+  }
+};
+
+export { associateEvent };
+
