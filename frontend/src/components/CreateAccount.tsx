dec68d26c8263ac08f334d708d8e7a565329fe3f
Facundo <facku995@gmail.com>
2024-03-05 11:19:41 -0300

    se conecto con backend

--- frontend/src/components/CreateAccount.tsx
+++ frontend/src/components/CreateAccount.tsx
@@ -2,8 +2,12 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons/faFacebook";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons/faGoogle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import CompleteAccount from "./CompleteAccount";
+import { RenderIf } from "./RenderIf";
+import DatosSegundoNivel from "./DatosSegundoNivel";
+import DatosExtras from "./DatosExtras";
+import axios from "axios";
 
 interface ICreateAccount {
   username: string;
@@ -19,16 +23,24 @@ interface IProps {
 export const CreateAccount = ({ newUser, setNewUser }: IProps) => {
   const [createAccount, setCreateAccount] = useState(true);
 
+  const [step, setStep] = useState(0);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<ICreateAccount>();
 
-  const handleCreateAccount = (data: ICreateAccount) => {
+  const handleCreateAccount = async (data: ICreateAccount) => {
     console.log(data);
-    if(data !== null){
+    try {
+      const response = await axios.post(
+        "https://c16-07-m-node-react.onrender.com/users/",
+        data
+      );
       setCreateAccount(!createAccount);
+    } catch (error) {
+      console.log("error al crear usuario", error);
     }
   };
 
@@ -36,8 +48,33 @@ export const CreateAccount = ({ newUser, setNewUser }: IProps) => {
     setNewUser(!newUser);
   };
 
+  const nextStep = () => {
+    if (step < 4) {
+      setStep(step + 1);
+    }
+  };
+
+  const prevStep = () => {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  };
+
   return (
     <div className="w-full flex flex-col gap-4 p-5">
+      <RenderIf condition={step > 0}>
+        <ul className="steps">
+          <li className={`step ${step >= 1 ? "step-primary" : ""}`}>
+            Datos principales
+          </li>
+          <li className={`step ${step >= 2 ? "step-primary" : ""}`}>
+            Datos adicionales
+          </li>
+          <li className={`step ${step >= 3 ? "step-primary" : ""}`}>
+            Datos Extra
+          </li>
+        </ul>
+      </RenderIf>
       {createAccount ? (
         <>
           <h1 className="text-center">Crear cuenta</h1>
@@ -66,7 +103,11 @@ export const CreateAccount = ({ newUser, setNewUser }: IProps) => {
               className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
             {errors.password && <span>Constraseña es requerida</span>}
-            <button type="submit" className="w-full btn btn-primary ">
+            <button
+              type="submit"
+              className="w-full btn bg-amber-400"
+              onClick={nextStep}
+            >
               Crear cuenta
             </button>
           </form>
@@ -92,8 +133,36 @@ export const CreateAccount = ({ newUser, setNewUser }: IProps) => {
           </div>
         </>
       ) : (
-        <CompleteAccount />
+        <>
+          <RenderIf condition={step === 1}>
+            <CompleteAccount />
+          </RenderIf>
+          <RenderIf condition={step === 2}>
+            <DatosSegundoNivel />
+          </RenderIf>
+          <RenderIf condition={step === 3}>
+            <DatosExtras />
+          </RenderIf>
+        </>
       )}
+      <div className="w-full flex flex-row justify-end gap-5">
+        <RenderIf condition={step !== 0}>
+          <button className="btn btn-outline text-blue-500">
+            Omitir ese paso
+          </button>
+          <button onClick={prevStep} className="btn bg-amber-400">
+            Anterior
+          </button>
+        </RenderIf>
+        <RenderIf condition={step <= 2}>
+          <button className="btn bg-amber-400" onClick={nextStep}>
+            Siguiente
+          </button>
+        </RenderIf>
+        <RenderIf condition={step === 3}>
+          <button className="btn bg-amber-400">Finalizar</button>
+        </RenderIf>
+      </div>
     </div>
   );
 };
