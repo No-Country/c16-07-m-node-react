d3186bda434261f6e867b5509ba56b958c50fe24
Nicolas Gómez <gomez.reyes.jhair@gmail.com>
2024-03-06 10:21:30 -0500

    feat: add route /perfil

--- frontend/src/route.config.ts
+++ frontend/src/route.config.ts
@@ -5,14 +5,16 @@ import Eventos from "./pages/Eventos";
 import Ayuda from "./pages/Ayuda";
 import Landing from "./pages/Landing";
 import Ejercicios from "./pages/Ejercicios";
+import Perfil from "./pages/Perfil/Perfil";
 
 const rutas = [
-   { path: "/oldMen/mostrar", element: MostrarOldman },
-   { path: "/", element: Home },
-   { path: "/Login", element: Login },
-   { path: "/Eventos", element: Eventos },
-   { path: "/Ayuda", element: Ayuda },
-   { path: "/Landing", element: Landing },
-   { path: "/Ejercicios", element: Ejercicios },
+  { path: "/oldMen/mostrar", element: MostrarOldman },
+  { path: "/", element: Home },
+  { path: "/Login", element: Login },
+  { path: "/Eventos", element: Eventos },
+  { path: "/Ayuda", element: Ayuda },
+  { path: "/Landing", element: Landing },
+  { path: "/Perfil", element: Perfil },
+  { path: "/Ejercicios", element: Ejercicios }
 ];
 export default rutas;
